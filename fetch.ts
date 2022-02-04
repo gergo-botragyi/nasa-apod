96b6092ff69fcef0d055b89ac4a1adb9aa0e387c
botragyi-gergo <botragyi.gergo@gmail.com>
2022-02-04 12:34:58 +0100

    frontend

--- fetch.ts
+++ fetch.ts
@@ -1,12 +1,9 @@
 import fetch from "node-fetch";
 
-const API_KEY = "XaqDXc4hfpi7n215Gn3Shi99eftlo81HT1Kq1VWO";
-
-const response = async (date?:string|undefined):Promise<object> => {
-    const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${date !== undefined && date.match(/^\d{4}-\d{2}-\d{2}$/gm) ? `&date=${date}` : `&count=1`}`,{
+const response = async (date?:string|undefined, api_key?:string|undefined):Promise<object> => {
+    const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}${date !== undefined && date.match(/^\d{4}-\d{2}-\d{2}$/gm) ? `&date=${date}` : `&count=1`}`,{
         method:"GET"
     });
-    console.log(data);
     if(!data.ok||data.status!=200)
     {return {error:data.statusText}}
     return {result:(await data.json()) as object};
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,26 +7,35 @@ import { resourceLimits } from 'worker_threads'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  const [asd, setAsd] = useState("");
+  const [about, setAbout] = useState("");
   const [resultPic, setResultPic] = useState("")
   const input = useRef<HTMLInputElement>(null);
+  const [spin, setSpin] = useState(false)
   const getapod = async (randpic:boolean) =>{
+    setSpin(true)
     const response = await fetch(`/api/hello?date=${randpic ? "rand": input.current?.value}`);    
-    const data = await response.json();
+    const data = await response.json();    
     const {result,error} = data;
     if(error!==undefined)
     {alert(`ERROR:${error}`);}
-    setAsd(result.constructor === Array ? result[0].explanation : result.explanation);    
+    setAbout(result.constructor === Array ? result[0].explanation : result.explanation);    
     setResultPic(result.constructor === Array ? result[0].url : result.url);
-    console.log(result.constructor === Array ? result[0].url : result.url);
+    setSpin(false);
   }
   return (
     <div>
-      <p>{asd}</p>
-      <button onClick={()=>{getapod(true)}}>Get random picture</button>
-      <input type="text" placeholder='YYYY-MM-DD' ref={input}/>
-      <button onClick={()=>{getapod(false)}}>Get picture</button>
-      <img src={resultPic} alt="" />
+      <p className='text-center font-bold text-3xl m-2 text-zinc-200'>NASA Astronomy Picture of the Day API</p>
+      <div className='flex text-center justify-center'>
+        <button onClick={()=>{getapod(true)}} className='button'>Get random picture</button>
+        <input type="text" placeholder='YYYY-MM-DD' ref={input} className='input'/>
+        <button onClick={()=>{getapod(false)}} className='button'>Get picture</button>
+      </div>      
+      <div className={spin ? 'flex justify-center items-center' : 'hidden'}>
+        <div className='animate-spin w-8 h-8 border-4 rounded-xl text-black m-2'></div>
+        <span className='text-zinc-200'>Loading...</span>
+      </div>
+      <div className='flex justify-center text-center m-2'><img src={resultPic} className='border-2 border-zinc-200' /></div>
+      <div className='flex justify-center text-center m-2'><p className='w-2/3 text-lg text-zinc-200'>{about}</p></div>      
     </div>
   )
 }
